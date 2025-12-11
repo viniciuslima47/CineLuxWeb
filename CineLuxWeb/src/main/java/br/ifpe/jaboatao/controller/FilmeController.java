c2698d08095420ce586a28b2ed0d7ed2370a3c9c
Gisela Araújo <purpuraverde02@gmail.com>
2025-12-11 10:36:06 -0300

    Add files via upload

--- CineLuxWeb/src/main/java/br/ifpe/jaboatao/controller/FilmeController.java
+++ CineLuxWeb/src/main/java/br/ifpe/jaboatao/controller/FilmeController.java
@@ -2,33 +2,60 @@ package br.ifpe.jaboatao.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import br.ifpe.jaboatao.model.Filme;
 import br.ifpe.jaboatao.repository.FilmeRepository;
+import org.springframework.validation.BindingResult;
+
 import jakarta.validation.Valid;
 
+
+
 @Controller
+@RequestMapping("/filme")
 public class FilmeController {
 
     @Autowired
     private FilmeRepository fr;
     
-    @GetMapping("/filme")
-    public String formularioFilme(Filme filme) {
-        return "filme";
+    // listar filmes
+    @GetMapping
+    public String listar(Model model){
+        model.addAttribute("listarfilmes", fr.findAll());
+        return "filme-lista";
     }
 
-    @PostMapping("/filme")
-    public String cadastrarFilme(@Valid Filme filme, BindingResult result) {
+   // form cadastrar
+    @GetMapping("/novo")
+    public String formCadastrar(Filme filme){
+        return "filme-form"; // exibe formulário
+    }
 
-        if (result.hasErrors()) {
-            return "filme";
+    // salvar (create / update)
+    @PostMapping("/salvar")
+    public String salvar(@Valid Filme filme, BindingResult result) {
+        if(result.hasErrors()) {
+            return "filme-form";
         }
-
         fr.save(filme);
-        return "redirect:/";
+        return "redirect:/filme";
     }
+
+     // editar
+    @GetMapping("/editar/{id}")
+    public String editar(@PathVariable Integer id, Model model) {
+        Filme filme = fr.findById(id).orElse(null);
+        model.addAttribute("filme", filme);
+        return "filme-form";
+    }
+
+    // deletar
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id){
+        fr.deleteById(id);
+        return "redirect:/filme";
+    }
+    
 }
---
+++ CineLuxWeb/src/main/java/br/ifpe/jaboatao/repository/FuncionarioRepository.java
@@ -0,0 +1,11 @@
+package br.ifpe.jaboatao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.ifpe.jaboatao.model.Funcionario;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
+
+}
